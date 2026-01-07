9da6d77f3a90f5cff99f78549af6b4b01463e71e
deepjadeja904-del <deepjadeja904@gmail.com>
2026-01-07 09:01:26 +0530

    Add files via upload

---
+++ datatype.oy.py
@@ -0,0 +1,51 @@
+x=10
+print(x)
+
+a=5
+print(-a)
+
+a=10
+b=15
+print(a+b)
+
+a=10
+b=15
+print(a-b)
+
+a=10
+b=15
+print(a*b)
+
+a=10
+b=15
+print(a/b)
+
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+is_pass=True
+print(is_pass)
+
+a=5
+b=3
+print(a&b)
+print(a|b)
+
+numbers =[123456]
+print(2 in numbers)
+print(5 not in numbers)
+
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
+
