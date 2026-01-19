a6b80f39a0d9d1d815c11def0197dcaf1a9b3e0e
deepjadeja904-del <deepjadeja904@gmail.com>
2026-01-19 10:35:25 +0530

    Add files via upload

---
+++ assig11.py
@@ -0,0 +1,67 @@
+print("welcome to python program")
+
+print("\tRujesh Kumar")
+print("\tFlat no. 101, Sunshine Apartments")
+print("\tMG Road, Sector 15")
+print("\tRajkot")
+print("\tPincode : 360005")
+print("\tIndia")
+
+A = 150
+b = 120.50
+print("Addition =", A + b)
+print("Subtraction =", A - b)
+print("Multiplication =", A* b)
+print("Division =", A / b)
+
+Radius = 7
+
+area = 3.14 * Radius * Radius
+circumference = 2 * 3.14 * Radius
+
+print("Area of Circle =", area)
+print("Circumference of Circle =", circumference)
+
+p=100
+r=5
+t=2
+
+si=(p*r*t)/100
+print("simple insterest =",si)
+
+length=10
+width=5
+
+perimeter=2*(length +width)
+print("perimeter of rectangle =",perimeter)
+
+length = 10
+width = 5
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area of Rectangle =", area)
+print("Perimeter of Rectangle =", perimeter)
+
+
+a = 3
+b = 4
+c = 5
+
+perimeter = a + b + c
+print("Perimeter of Triangle =", perimeter)
+
+
+side = 6
+
+area = side * side
+perimeter = 4 * side
+
+print("Area of Square =", area)
+print("Perimeter of Square =", perimeter)
+
+side = 6
+
+perimeter = 4 * side
+print("Perimeter of Square =", perimeter)
