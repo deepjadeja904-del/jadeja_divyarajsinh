6da5ca7b15ad0f8d3cd154a37871fdb4b96fea32
deepjadeja904-del <deepjadeja904@gmail.com>
2026-02-02 10:50:03 +0530

    Add files via upload

---
+++ I O.py
@@ -0,0 +1,34 @@
+#1 str
+a=("hellow world")
+print(a)
+
+b=("welcome world")
+print(b)
+
+#2 int
+int = input("enter no:-")
+print("The no. is ",int)
+
+
+int = input("enter no:-")
+print("the no is ",int)
+
+#3 flot
+x=5
+y=6
+
+print("the sum of x and y : ",x+y)
+
+c=10
+d=10
+print("the sum of x and y : ",c*d)
+
+#4 bool
+e=9
+f=3
+print(e>f)
+
+
+g=10
+h=11
+print(g<h)
