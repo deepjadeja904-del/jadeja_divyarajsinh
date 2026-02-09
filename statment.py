a713a8523860717a7be667159d31a0ab17a84921
deepjadeja904-del <deepjadeja904@gmail.com>
2026-02-09 10:29:31 +0530

    Add files via upload

---
+++ statment.py
@@ -0,0 +1,37 @@
+#1
+age=int(input("Enter your age="))
+if age>=18:
+    print("yes you are eligible for voting...||")
+else:
+     print("no. you are eligible for voting...||")
+
+#2
+marks=int(input("Enter your marks="))
+if marks>=90:
+    print("you are have A grade")
+elif marks>=75:
+    print("you are have B grade")    
+else:
+     print("you arer have C grade")
+
+
+#3
+number=int(input("Enter your number="))
+if number % 2 == 0:
+    print("the number is even")
+else:
+     print("the number is odd")
+
+#4
+number=int(input("Enter your age="))
+if number > 0:
+    print("the number is positive")
+else:
+     print("the number is nagative")
+
+#5
+num=int(input("Enter your age="))
+if age>=18:
+    print("yes you are eligible for licence...||")
+else:
+     print("no. you are eligible for licence...||")
