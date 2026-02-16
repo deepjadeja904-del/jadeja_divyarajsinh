7cae9a5170f15f49a0c54061f2ef3ab03d7f287e
deepjadeja904-del <deepjadeja904@gmail.com>
2026-02-16 10:28:11 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,41 @@
+#1
+for i in range (1,6):
+    print(i)
+    
+#2
+for i in range (3):
+    print("hello")
+    
+#3
+for i in range (1,11):
+    print(i)    
+    
+#4
+for i in range (1,21):
+    if i % 2 == 0:
+        print(i)
+        
+#5
+for i in range (1,21):
+    if i % 2 !=0:
+        print(i)
+        
+#6
+for i in range(1,11):
+    print("5 x",i,"=",5*i)
+    
+#7
+name="hello"
+for letter  in name:
+    print(letter)
+    
+#8
+total = 0
+for i in range (1,6):
+    total += i 
+    print("sum of number from 1 to 5",total)
+    
+#9 
+number = [10,20,30,40,50]
+for num in number:
+    print(num)                  
